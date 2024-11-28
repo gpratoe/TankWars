4342c7f54aae7e6daf675d2b6568cb7987afab5e
gpratoe <gaston.prato@mi.unc.edu.ar>
2024-11-28 01:03:19 -0300

    bullets v1

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -38,25 +38,28 @@ class Game {
         const tank2h = 50;
 
         this.tanks.push(new Tank("Player 1", 0x00FF00, -this.width/2 + tank1w/2, 0, tank1w, tank1h, 0, 10, 10));
-        this.tanks.push(new Tank("Player 2", 0xFF0000, this.width/2 - tank2w/2, 0, tank2w, tank2h, 0, 10, 10));
+        //this.tanks.push(new Tank("Player 2", 0xFF0000, this.width/2 - tank2w/2, 0, tank2w, tank2h, 0, 10, 10));
     }
 
     update() {
         this.app.stage.on("pointermove", (event) => {
             const mousePos = event.global;
     
-            this.#mouseX = mousePos.x;
-            this.#mouseY = mousePos.y;
+            this.mouseX = mousePos.x;
+            this.mouseY = mousePos.y;
         });
+        this.isMouseDown = false
+        this.app.stage.on("mousedown", (event) => {
+            this.isMouseDown = true
+        })
+        this.app.stage.on("mouseup", (event) => {
+            this.isMouseDown = false
 
+        })
         this.app.ticker.add(() => {
             for (let i = 0; i < this.tanks.length; i++){
-                const dx = this.#mouseX - this.tanks[i].container.x;
-                const dy = this.#mouseY - this.tanks[i].container.y;
-                
-                const angle = Math.atan2(dy,dx);
     
-                this.tanks[i].update(angle);
+                this.tanks[i].update();
             }
     
         })
@@ -64,5 +67,5 @@ class Game {
     }
 }
 
-const game = new Game(800,600);
+const game = new Game(1900,1080);
 export {game}; 
--- frontend/src/tank.js
+++ frontend/src/tank.js
@@ -1,6 +1,7 @@
 import { Graphics, Container } from "pixi.js";
 import { game } from "./game";
 import { HealthBar } from "./healthbar";
+import { Bullet } from "./bullet";
 
 class Tank {
     constructor(name, color, x, y, w , h, angle, damage, bullet_speed)
@@ -16,6 +17,7 @@ class Tank {
         this.damage = damage;
         this.bullet_speed = bullet_speed;
         this.#setup_container();
+        this.bullets = []
        // this.#setup();
     }
 
@@ -53,11 +55,63 @@ class Tank {
             this.container.pivot.set(this.container.width/2, this.container.height/2);
     }
 
-    update(angle){
-
-        this.angle = angle;
+    update(){
+        const dx = game.mouseX - this.container.x;
+        const dy = game.mouseY - this.container.y;
+                
+        this.angle = Math.atan2(dy,dx);
         
         this.container.rotation = this.angle;
+
+
+        const mag = Math.sqrt(dx * dx + dy * dy);
+        const topSpeed = 15;
+        const bulletSpeed = 17;
+        if( mag >= this.container.width && !game.isMouseDown ){        
+            
+            const normDx = dx / mag;
+            const normDy = dy / mag;
+
+            let ds = (mag*5) / this.container.width;
+            const speed =  ds > topSpeed ? topSpeed : ds ;
+
+            this.container.x += normDx * speed;
+            this.container.y += normDy * speed;
+        
+        }
+        
+        if(game.isMouseDown){
+            const bullet = new Graphics()
+                .rect(0,0,5,5)
+                .fill({
+                    color: 0xFF00FF,
+                    alpha: 1
+                    })
+                .stroke({
+                    width: 2,
+                    color: 0x000000
+                    });
+
+            bullet.pivot.set(bullet.width/2, bullet.height/2);
+            bullet.position.x = this.container.position.x;
+            bullet.position.y = this.container.position.y;
+            this.bullets.push([bullet, this.angle]);
+            this.bullets.forEach(bullet => {
+                game.app.stage.addChild(bullet[0]);
+            });
+        }
+
+        for(let i = 0; i < this.bullets.length; i++) {
+            if (this.bullets[i][0].x > game.app.screen.width || this.bullets[i][0].x < 0 || 
+                this.bullets[i][0].y > game.app.screen.height || this.bullets[i][0].y < 0) {
+
+                this.bullets[i][0].destroy();
+                this.bullets.splice(i,1);
+            } else {
+                this.bullets[i][0].x += bulletSpeed*Math.cos(this.bullets[i][1]);
+                this.bullets[i][0].y += bulletSpeed*Math.sin(this.bullets[i][1]);
+            }
+        }
     }
 }
 
