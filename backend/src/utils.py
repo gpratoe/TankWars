20d6da346ac3a9767e03c4c8a659f0ad8502fb5b
Tato <tatoprato102@gmail.com>
2024-12-13 12:22:40 -0300

    utils class, for basic convertions between box2d world and pixels and common useful data

---
+++ backend/src/utils.py
@@ -0,0 +1,25 @@
+from Box2D import b2Vec2
+
+class Utils:
+    def __init__(self):
+        self.world = None
+        self.gameHeight = 0
+        self.gameWidth = 0
+        self.PPM = 10
+        self.mouseX = 0
+        self.mouseY = 0
+
+
+    def to_pixel(self, meter):
+        return meter * self.PPM
+    
+    def to_world(self, pixel):
+        return pixel / self.PPM
+    
+    def vec2_to_pixel(self, vec2):
+        return b2Vec2(self.to_pixel(vec2[0]), self.to_pixel(vec2[1]))
+    
+    def vec2_to_world(self, vec2):
+        return b2Vec2(self.to_world(vec2[0]), self.to_world(vec2[1]))
+
+utils = Utils()
