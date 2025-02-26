6f524c0918ac52485ce6da957444f73b89c91a9e
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-02-26 01:04:36 -0300

    basic game endpoints

--- backend/api/routers/game_router.py
+++ backend/api/routers/game_router.py
@@ -1,8 +1,73 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, status, HTTPException
+from pydantic import BaseModel
 from api.ws import manager
-
+from db.game_handler import GameHandler
 
 gr = APIRouter()
+gh = GameHandler()
+
+class GameSchema(BaseModel):
+    name: str
+    max_players: int
+    owner_id: int
+
+@gr.post(path="", status_code=status.HTTP_201_CREATED)
+async def create_game(game_data: GameSchema):
+    try:
+        game = gh.create_game(game_data.name, game_data.max_players, game_data.owner_id)
+        return game.to_dict()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+@gr.get(path="/{id}", status_code=status.HTTP_200_OK)
+async def get_game(id:int):
+    try:
+        game = gh.get_game(id)
+        return game.to_dict()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+@gr.get(path="/{id}/players", status_code=status.HTTP_200_OK)
+async def get_game_players(id:int):
+    try:
+        players = gh.get_game_players(id)
+        return players
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+@gr.get(path="", status_code=status.HTTP_200_OK)
+async def get_games(lobby:bool=False):
+    if lobby:
+        try:
+            lobbies = gh.get_lobbies()
+            return lobbies
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    else:
+        return []
+        
+@gr.post(path="/{game_id}/players", status_code=status.HTTP_202_ACCEPTED)
+async def add_player_to_game(game_id:int, player_id:int):
+    try:
+        game = gh.add_player_to_game(game_id, player_id)
+        return game.to_dict()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+@gr.delete("/{game_id}/players/{player_id}",status_code=status.HTTP_202_ACCEPTED)
+async def remove_player_from_game(game_id:int, player_id:int):
+    try:
+        game = gh.remove_player_from_game(game_id, player_id)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+@gr.post("/{game_id}/start", status_code=status.HTTP_202_ACCEPTED)
+async def start_game(game_id:int, owner_id:int):
+    try:
+        game = gh.start_game(game_id, owner_id)
+        return game.to_dict()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 @gr.websocket("/testws")
 async def websocket_endpoint(websocket: WebSocket, name: str):
---
+++ backend/db/game_handler.py
@@ -0,0 +1,118 @@
+from db.models import Player, Game
+from pony.orm import db_session
+
+colors = ['orange', 'blue', 'green', 'yellow']
+
+class GameHandler:
+
+    @db_session
+    def create_game(self, name, max_players, owner_id):
+        if max_players < 2 or max_players > 4:
+            raise ValueError('Max players must be between 2 and 4')
+        
+        player = Player.get(id=owner_id)
+        
+        if player is None:
+            raise ValueError(f'Player with id {owner_id} not found')
+        if player.game is not None:
+            raise ValueError(f'Player with id {owner_id} is already in a game')
+        if player.is_owner:
+            raise ValueError(f'Player with id {owner_id} is already a game owner')
+        
+        game = Game(name=name, max_players=max_players)
+        game.players.add(player)
+        player.is_owner = True
+        player.color = colors[0]
+        return game
+    
+    @db_session
+    def get_game(self, id):
+        game = Game.get(id=id)
+        if game is None:
+            raise ValueError(f'Game with id {id} not found')
+        return game
+
+    @db_session
+    def get_game_players(self, game_id):
+        game = Game.get(id=game_id)
+        if game is None:
+            raise ValueError(f'Game with id {game_id} not found')
+        players = game.players
+        return [p.to_dict() for p in players]
+    
+
+    @db_session
+    def get_games(self, lobby=False):
+        if lobby:
+            lobbies = Game.select(lambda g: g.in_lobby)
+            return [l.to_dict() for l in lobbies]
+        else:
+            games = Game.select(lambda g: not g.in_lobby)
+            return [g.to_dict() for g in games]
+    
+    @db_session
+    def add_player_to_game(self, game_id, player_id):
+        game = Game.get(id=game_id)
+        if game is None:
+            raise ValueError(f'Game with id {game_id} not found')
+        
+        player = Player.get(id=player_id)
+        if player is None:
+            raise ValueError(f'Player with id {player_id} not found')
+        
+        if player.game is not None:
+            raise ValueError(f'Player with id {player_id} is already in a game')
+        
+        if game.in_lobby:
+            game.players.add(player)
+            player.color = colors[len(game.players)]
+            return game
+        else:
+            raise ValueError(f'Game with id {game_id} is not in lobby')
+        
+    @db_session
+    def remove_player_from_game(self, game_id, player_id):
+        game = Game.get(id=game_id)
+        if game is None:
+            raise ValueError(f'Game with id {game_id} not found')
+    
+        player = Player.get(id=player_id)
+        if player is None:
+            raise ValueError(f'Player with id {player_id} not found')
+        if player.game is None:
+            raise ValueError(f'Player with id {player_id} is not in a game')
+        if player.game.id != game.id:
+            raise ValueError(f'Player with id {player_id} is not in game with id {game_id}')
+        
+        game.players.remove(player)
+        player.color = ""
+        if player.is_owner:
+            player.is_owner = False
+            if len(game.players) > 0:
+                new_owner = game.players.first()
+                new_owner.is_owner = True
+                for player in game.players:
+                    i=0
+                    player.color = colors[i]
+                    i+=1
+            else:
+                game.delete()        
+
+    @db_session
+    def start_game(self, game_id, owner_id):
+        game = Game.get(id=game_id)
+        players: set = game.players
+
+        if game is None:
+            raise ValueError(f'Game with id {game_id} not found')
+        if players.__len__() < 2:
+            raise ValueError(f'Not enough players in game with id {game_id}')
+        
+        owner = Player.get(id=owner_id)
+        if owner is None:
+            raise ValueError(f'Player with id {owner_id} not found')
+        if not owner.is_owner or owner.game.id != game.id:
+            raise ValueError(f'Player with id {owner_id} is not the owner of game with id {game_id}')
+        
+        game.in_lobby = False
+        return game
