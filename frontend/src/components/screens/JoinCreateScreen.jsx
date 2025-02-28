db839cb1c14f2e0b4ff217c8cfda0cbaa8e574e3
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-02-27 23:38:00 -0300

    basic lobbies screen

---
+++ frontend/src/components/screens/JoinCreateScreen.jsx
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { get_lobbies, join_game } from '../../apiService';
+import '../../styles/JoinCreateScreen.css';
+import CreateGameForm from '../CreateGameForm';
+
+function JoinCreateScreen({}) {
+  const [lobbies, setLobbies] = useState([]);
+  const [createGame, setCreateGame] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetch_lobbies(){
+      try{
+        const data = await get_lobbies();
+        setLobbies(data);
+        console.log(data);
+      }
+      catch(err){
+        console.error(err);
+      }
+    }
+
+    fetch_lobbies();
+  }, []);
+
+  const handleJoinGame = async (lobby_id) => {
+    const playerId = sessionStorage.getItem('playerId');
+    if (playerId){
+      try {
+        await join_game(lobby_id, playerId);
+        navigate(`/game/${lobby_id}`);
+      }
+      catch(err){
+        console.error(err);
+      }
+    }
+  }
+
+  return (
+    <div className="join-create-screen">
+      
+      { createGame ? (
+        <div>
+          <CreateGameForm goBackFunc={setCreateGame}/>
+        </div>
+      ):(
+        <>
+          <h1>Salas</h1>
+          <button className='green-button' id='boton-crear' onClick = {() => setCreateGame(true)}>Crear sala</button>
+          <ul>
+            {lobbies.map((lobby, i) =>
+            <li key={i}>
+              <p>{lobby.name} (#{lobby.id}) - Max players:<span style={{color: 'red'}}>{lobby.max_players}</span></p>
+              <button className='green-button' onClick={() => {handleJoinGame(lobby.id)}}>Unirse</button>
+            </li>  
+            )}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default JoinCreateScreen;
