75050d66e424603357e4359a12ec1396f7cfac4a
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-02-27 23:39:02 -0300

    api fetching functions

---
+++ frontend/src/apiService.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+export const API_URL = 'http://localhost:8000';
+
+export async function create_player(name) {
+    try{
+      const response = await axios.post(`${API_URL}/player?name=${name}`)
+      if(response.status === 201){
+        return response.data;
+      }
+      else {
+        throw new Error(`Error al crear el jugador: ${response.statusText}`);
+      }
+    }
+    catch (error){
+      throw new Error(`Error al crear el jugador: ${error.message}`);
+    }
+  }
+
+export async function get_lobbies() {
+    try{
+        const response = await axios.get(`${API_URL}/game?lobby=true`);
+        if (response.status === 200){
+            return response.data;
+        }
+        else {
+            throw new Error(`Error al obtener las partidas: ${response.statusText}`);
+        }
+    }
+    catch (error){
+        throw new Error(`Error al obtener las partidas: ${error.message}`);
+    }
+}
+
+export async function join_game(lobby_id, player_id) {
+    try{
+        const response = await axios.post(`${API_URL}/game/${lobby_id}/players?player_id=${player_id}`);
+        if (response.status === 202){
+            return response.data;
+        }
+        else {
+            throw new Error(`Error al unirse a la partida: ${response.statusText}`);
+        }
+    }
+    catch (error){
+        throw new Error(`Error al unirse a la partida: ${error.message}`);
+    }
+}
+
+export async function create_game(name, max_players, owner_id) {
+    try{
+        const body = {
+            name: name,
+            max_players: max_players,
+            owner_id: owner_id
+        }
+        const response = await axios.post(`${API_URL}/game`, body);
+        if (response.status === 201){
+            return response.data;
+        }
+        else {
+            throw new Error(`Error al crear la partida: ${response.statusText}`);
+        }
+    }
+    catch (error){
+        throw new Error(`Error al crear la partida: ${error.message}`);
+    }
+}
