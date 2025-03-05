4ab0f536256801896757162a9009b13554ff821c
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-03-05 01:46:33 -0300

    basic ws context

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -5,32 +5,35 @@ import JoinCreateScreen from './screens/JoinCreateScreen';
 import GameScreen from './screens/GameScreen';
 import LobbyScreen from './screens/LobbyScreen';
 import '../styles/App.css';
+import { WebSocketProvider } from '../webSocketContext';
 
 function App() {
 
   return (
     <div style={{ fontFamily: 'Arial', textAlign: 'center' }}>
-      <Routes>
-        <Route
-          path="/"
-          element={<NameScreen />}
-        />
-        <Route
-          path="/lobby"
-          element={
-            <JoinCreateScreen/>
-          }
-        />
-        <Route
-          path="/lobby/:lobbyId"
-          element={<LobbyScreen />}
-        />
-        <Route
-          path="/game/:lobbyId"
-          element={<GameScreen />}
-        />
-        
-      </Routes>
+      <WebSocketProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={<NameScreen />}
+          />
+          <Route
+            path="/lobby"
+            element={
+              <JoinCreateScreen/>
+            }
+          />
+          <Route
+            path="/lobby/:lobbyId"
+            element={<LobbyScreen />}
+          />
+          <Route
+            path="/game/:lobbyId"
+            element={<GameScreen />}
+          />
+          
+        </Routes>
+      </WebSocketProvider>
     </div>
   );
 }
---
+++ frontend/src/webSocketContext.jsx
@@ -0,0 +1,78 @@
+import { createContext, useEffect, useRef, useContext } from "react";
+import { on } from "ws";
+
+export const WebSocketContext = createContext(undefined);
+
+export function WebSocketProvider({ children }) {
+    const webSockets = useRef({});
+    const callBacks = useRef({});
+
+    const subscribe = (url, callBack) => {
+        if (!callBacks.current[url]) {
+            callBacks.current[url] = [];
+        }
+        callBacks.current[url].push(callBack);
+    }
+
+    const createWebSocket = (url) => {
+        const ws = new WebSocket(url);
+        ws.onopen = () => {
+            console.log(`Connected to ${url}`);
+            ws.send(JSON.stringify({ event: 'connect' }));
+        };
+
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log(callBacks.current[url]);
+            if (callBacks.current[url]) {
+                callBacks.current[url].forEach((cb) => {
+                    cb(data);
+                });
+            }
+        };
+
+        return ws;
+    };
+
+    const getWebSocket = (url) => {
+        if (!webSockets.current[url]) {
+            webSockets.current[url] = createWebSocket(url);
+            callBacks.current[url] = [];
+        }
+        return webSockets.current[url];
+    };
+
+    // cleanup
+    useEffect(() => {
+        return () => {
+            Object.values(webSockets.current).forEach(ws => ws.close());
+            webSockets.current = {};
+            callBacks.current = {};
+        };
+    }, []);
+
+    return (
+        <WebSocketContext.Provider value ={{ getWebSocket, subscribe }}>
+            {children}
+        </WebSocketContext.Provider>
+    );
+}
+
+// custom hook to get the websocket
+export function useWebSocket(url, onMessage) {
+    const context = useContext(WebSocketContext);
+    
+    if (!context) {
+        throw new Error('Not in provider wachin');
+    }
+    
+    const { getWebSocket, subscribe} = context;
+    const ws = getWebSocket(url);
+
+    useEffect(()=>{
+        if (onMessage) {
+            subscribe(url, onMessage);
+        }
+    }, [url, onMessage, subscribe]);
+    return ws;
+}
