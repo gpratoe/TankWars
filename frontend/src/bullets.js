d74c14fd3a95ae1847a742bf5e7faa8b1cf296b5
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-03-08 04:50:02 -0300

    rm game import, inject app dependency

--- frontend/src/bullets.js
+++ frontend/src/bullets.js
@@ -1,7 +1,9 @@
 import { Graphics } from "pixi.js";
-import {game} from "./game";
+
 class Bullets {
-    constructor(x,y,dir_norm,damage,speed){
+    constructor(x,y,dir_norm,damage,speed, app){
+        this.damage = damage;
+        this.app = app;
         this.speed = speed;
         this.dir_norm = dir_norm;
         this.bullet = new Graphics()
@@ -10,7 +12,7 @@ class Bullets {
         this.bullet.pivot.set(this.bullet.width/2, this.bullet.height/2);
         this.bullet.position.x = x;
         this.bullet.position.y = y;
-        game.app.stage.addChild(this.bullet);
+        app.stage.addChild(this.bullet);
     }
 }
 
