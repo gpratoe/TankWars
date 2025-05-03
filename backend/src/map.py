d081ed73c185ab36f231f367227f2887acc2f732
Tato <tatoprato102@gmail.com>
2025-05-03 06:55:35 -0300

    change bases for walls

--- backend/src/map.py
+++ backend/src/map.py
@@ -2,6 +2,7 @@ from src.physics_manager import PhysicsManager, BodyType
 from src.utils import utils
 from Box2D import b2FixtureDef, b2PolygonShape
 from src.settings import *
+from wall import Wall
 
 class Map:
     def __init__(self, physics_manager: PhysicsManager, w=100, h=100):
@@ -58,46 +59,31 @@ class Map:
         # |   |             |
         # ----|--------------
 
-        self.physics_manager.create_body(
-            body_type=BodyType.static,
-            position=utils.vec2_to_world((BASES_TLEFT_X, BASES_TLEFT_Y)),
-            fixture_def=b2FixtureDef(
-                shape=b2PolygonShape(box=utils.vec2_to_world((BASES_TLEFT_BRIGHT_WIDTH/2,
-                                                              BASES_TLEFT_BRIGHT_HEIGHT/2))),
-                density=0,
-                friction=0
-            )
+        Wall(
+            x=utils.to_pixel(BASES_TLEFT_X),
+            y=utils.to_pixel(BASES_TLEFT_Y),
+            width=utils.to_pixel(BASES_TLEFT_BRIGHT_WIDTH),
+            height=utils.to_pixel(BASES_TLEFT_BRIGHT_HEIGHT),
+            physics_manager=self.physics_manager
         )
-
-        self.physics_manager.create_body(
-            body_type=BodyType.static,
-            position=utils.vec2_to_world((BASES_BRIGHT_X, BASES_BRIGHT_Y)),
-            fixture_def=b2FixtureDef(
-                shape=b2PolygonShape(box=utils.vec2_to_world((BASES_TLEFT_BRIGHT_WIDTH/2,
-                                                              BASES_TLEFT_BRIGHT_HEIGHT/2))),
-                density=0,
-                friction=0
-            )
+        Wall(
+            x=utils.to_pixel(BASES_BRIGHT_X),
+            y=utils.to_pixel(BASES_BRIGHT_Y),
+            width=utils.to_pixel(BASES_BLEFT_TRIGHT_WIDTH),
+            height=utils.to_pixel(BASES_BLEFT_TRIGHT_HEIGHT),
+            physics_manager=self.physics_manager
         )
-
-        self.physics_manager.create_body(
-            body_type=BodyType.static,
-            position=utils.vec2_to_world((BASES_TRIGHT_X, BASES_TRIGHT_Y)),
-            fixture_def=b2FixtureDef(
-                shape=b2PolygonShape(box=utils.vec2_to_world((BASES_BLEFT_TRIGHT_WIDTH/2,
-                                                              BASES_BLEFT_TRIGHT_HEIGHT/2))),
-                density=0,
-                friction=0
-            )
+        Wall(
+            x=utils.to_pixel(BASES_BLEFT_X),
+            y=utils.to_pixel(BASES_BLEFT_Y),
+            width=utils.to_pixel(BASES_BLEFT_TRIGHT_WIDTH),
+            height=utils.to_pixel(BASES_BLEFT_TRIGHT_HEIGHT),
+            physics_manager=self.physics_manager
         )
-
-        self.physics_manager.create_body(
-            body_type=BodyType.static,
-            position=utils.vec2_to_world((BASES_BLEFT_X, BASES_BLEFT_Y)),
-            fixture_def=b2FixtureDef(
-                shape=b2PolygonShape(box=utils.vec2_to_world((BASES_BLEFT_TRIGHT_WIDTH/2,
-                                                              BASES_BLEFT_TRIGHT_HEIGHT/2))),
-                density=0,
-                friction=0
-            )
+        Wall(
+            x=utils.to_pixel(BASES_BLEFT_X),
+            y=utils.to_pixel(BASES_BLEFT_Y),
+            width=utils.to_pixel(BASES_BLEFT_TRIGHT_WIDTH),
+            height=utils.to_pixel(BASES_BLEFT_TRIGHT_HEIGHT),
+            physics_manager=self.physics_manager
         )
