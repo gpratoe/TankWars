7ea4ec2b5c177ed3b32fecdffb22b7b7d6043d70
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-05-06 13:54:07 -0300

    local and remote tanks different behaviour

--- frontend/src/tank.js
+++ frontend/src/tank.js
@@ -3,7 +3,7 @@ import { HealthBar } from "./healthbar";
 import { Bullets } from "./bullets";
 
 class Tank {
-    constructor(player_id, name, color, x, y, w , h, angle, damage, bullet_speed, app)
+    constructor(player_id, name, color, x, y, w , h, angle, damage, bullet_speed, app, is_local = false)
     {
         this.player_id = player_id
         this.app = app;
@@ -21,6 +21,13 @@ class Tank {
         this.isShooting = false;
         this.mouseX = x;
         this.mouseY = y;
+        this.is_local = is_local;
+        this.state_buffer = [{
+            x: x,
+            y: y,
+            angle: angle,
+            timestamp: Date.now()
+        }];
        // this.#setup();
     }
 
@@ -57,12 +64,19 @@ class Tank {
     }
     
     set_state(state){
+        const new_state = {
+            x: state.tankx,
+            y: state.tanky,
+            angle: state.angle,
+            timestamp: Date.now()
+        }
+        this.state_buffer.push(new_state);
+        this.state_buffer = this.state_buffer.filter(s => Date.now() - s.timestamp <= 200);
+
         this.x = state.tankx;
         this.y = state.tanky;
-        //this.container.x = state.tankx;
-        //this.container.y = state.tanky;
         this.angle = state.angle;
-        //this.container.rotation = state.angle;
+
         if(this.health > state.health){
             this.healthBar.decreaseHealth();
         }
@@ -78,11 +92,34 @@ class Tank {
         return start + diff * t; // Interpolar con la diferencia ajustada
     }
 
-    update() {
+    local_update(){
         let lerp_factor = 0.1;
         this.container.position.x += (this.x - this.container.position.x) * lerp_factor;
         this.container.position.y += (this.y - this.container.position.y) * lerp_factor;
         this.container.rotation = this.#lerpAngle(this.container.rotation, this.angle, lerp_factor);
+    }
+    
+    interpolate_from_buffer() {
+        if (this.state_buffer.length < 2) return;
+
+        const render_time = Date.now() - 50;
+        if (this.state_buffer[0].timestamp > render_time){
+            return;
+        }
+        
+        this.x = this.state_buffer[0].x;
+        this.y = this.state_buffer[0].y;
+        this.angle = this.state_buffer[0].angle;
+        this.local_update();
+        this.state_buffer.splice(0, 1);
+    }
+
+    update() {
+        if (this.is_local) {
+            this.local_update();
+        }else{
+            this.interpolate_from_buffer();
+        }
     }
 
     destroy() {
