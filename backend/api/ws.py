7e9a3c56024ab5c086d661d18822f3e9b9e51142
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-05-09 11:09:33 -0300

    make one ws manager per game to avoid conflicts

--- backend/api/ws.py
+++ backend/api/ws.py
@@ -3,34 +3,30 @@ from fastapi.websockets import WebSocket, WebSocketState
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: dict[int, dict[int, WebSocket]] = {}
+        self.active_connections: dict[int, WebSocket] = {}
 
     async def connect(self, connection: WebSocket, lobby_id: int, player_id: int):
         try:    
             await connection.accept()
-            if lobby_id not in self.active_connections:
-                self.active_connections[lobby_id] = {}
-            self.active_connections[lobby_id][player_id] = connection
+            self.active_connections[player_id] = connection
         
         except Exception as e:
             print(e, " conection failed")
-            if lobby_id in self.active_connections and player_id in self.active_connections[lobby_id]:
-                self.active_connections[lobby_id].pop(player_id)
+            if player_id in self.active_connections:
+                self.active_connections.pop(player_id)
 
     async def disconnect(self, lobby_id: int, player_id: int):
         try:
-           if lobby_id in self.active_connections and player_id in self.active_connections[lobby_id]:
-                if self.active_connections[lobby_id][player_id].client_state == WebSocketState.CONNECTED:
-                    await self.active_connections[lobby_id][player_id].close()
-                self.active_connections[lobby_id].pop(player_id)
+           if player_id in self.active_connections:
+                if self.active_connections[player_id].client_state == WebSocketState.CONNECTED:
+                    await self.active_connections[player_id].close()
+                self.active_connections.pop(player_id)
 
         except Exception as e:
             print(f"Error during disconnection of player with id: {player_id}: {e}")
 
     async def broadcast(self, data: dict, lobby_id: int):
-        if lobby_id not in self.active_connections:
-            return
-        connections_copy = self.active_connections[lobby_id].copy()
+        connections_copy = self.active_connections.copy()
         for player_id, connection in connections_copy.items():
             try:
                 await connection.send_json(data)
@@ -39,11 +35,11 @@ class ConnectionManager:
 
     async def send_message(self, data: dict, lobby_id: int, player_id: int):
         try:
-            if lobby_id in self.active_connections and player_id in self.active_connections[lobby_id]:
-                await self.active_connections[lobby_id][player_id].send_json(data)
+            if player_id in self.active_connections:
+                await self.active_connections[player_id].send_json(data)
         except Exception as e:
             print(f"Error sending message to player with id: {player_id} in lobby: {lobby_id}: {e}")
 
 
 
-manager = ConnectionManager()
+#manager = ConnectionManager()
