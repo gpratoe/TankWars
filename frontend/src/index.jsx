a9d44419dedd9a407e5ef0e3a65c8fe8833d7324
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-05-11 16:09:31 -0300

    updt

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,7 +6,7 @@ import App from './components/App.jsx';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename='/TankWars/frontend'>
+  <BrowserRouter basename='/'>
     <App />
   </BrowserRouter>
 );
