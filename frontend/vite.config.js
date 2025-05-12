71180aa9ac6ac81c9b08df7642e54cd7340f6171
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-05-12 12:44:56 -0300

    fix problem vite not loading env vars properly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
-});
+export default ({mode}) => {
+  
+  const env = loadEnv(mode, process.cwd());
+  console.log('BASE URL:', env.VITE_BASE_URL);
+  
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_BASE_URL || '/',
+  });
+
+}
