789dd1dbbd3d226033319a6ff805c63a57aa3eb8
gpratoe <gaston.prato@mi.unc.edu.ar>
2025-05-18 03:28:50 -0300

    little alg improvements

--- backend/src/light_collision_handler.py
+++ backend/src/light_collision_handler.py
@@ -7,31 +7,26 @@ class LP_CollisionHandler:
 
     def get_latest_collisions(self, tanks: list[LP_Tank], bullets: list[LP_Bullet], walls: list[LP_Wall]):
         collisions = set()
-        sublist = tanks.copy()
+
         for tank in tanks:
             for wall in walls:
                 if tank.circle_rect_collide(wall):
                     tank.bounce_on_rect(wall)
 
-        sublist = bullets.copy()
-        for bullet in bullets:
+        for i, bullet in enumerate(bullets):
             for wall in walls:
                 if bullet.circle_rect_collide(wall):
-                    new_collision = Collision(bullet, wall, CollisionType.BULLET_WALL)
-                    collisions.add(new_collision)
+                    collisions.add(Collision(bullet, wall, CollisionType.BULLET_WALL))
                     bullet.velocity = bullet.bounce_on_rect(wall)
 
             for tank in tanks:
                 if bullet.circle_circle_collide(tank):
-                    if bullet.groupIndex != tank.groupIndex:
-                        new_collision = Collision(bullet, tank, CollisionType.BULLET_TANK)
-                        collisions.add(new_collision)
+                    collisions.add(Collision(bullet, tank, CollisionType.BULLET_TANK))
 
-            sublist.remove(bullet)
-            for s_bullet in sublist:
-                if bullet.circle_circle_collide(s_bullet):
-                    new_collision = Collision(bullet, s_bullet, CollisionType.BULLET_BULLET)
-                    collisions.add(new_collision)
+            for j in range(i + 1, len(bullets)):
+                other = bullets[j]
+                if bullet.circle_circle_collide(other):
+                    collisions.add(Collision(bullet, other, CollisionType.BULLET_BULLET))
 
         if self.latest_collisions != collisions:
             self.latest_collisions = collisions
