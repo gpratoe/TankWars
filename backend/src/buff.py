e22a9a97b432d22635d8486451c8d57e375c582e
gpratoe <tatoprato102@gmail.com>
2025-06-08 12:01:41 -0300

    assssssssss

--- backend/src/buff.py
+++ backend/src/buff.py
@@ -1,14 +1,19 @@
 from abc import ABC, abstractmethod
 
 class Buff(ABC):
+    def __init__(self, id):
+        self.id = id
     @abstractmethod
     def apply(self, tank):
         pass
 
 class CoolDownBuff(Buff):
-    def __init__(self, id):
-        self.id = id
-
     def apply(self, tank):
         tank.cooldown /= 2
 
+class HealthBuff(Buff):
+    def apply(self, tank):
+        if tank.health >= 90:
+            tank.health = 100
+            return
+        tank.health += 10
